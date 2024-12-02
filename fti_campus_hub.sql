4691892979590fe7fd72c18ebbe21f75ff0079c2
Benni Putra Chaniago <bennichaniago2003@gmail.com>
2024-12-02 15:54:46 +0700

    sql database

---
+++ fti_campus_hub.sql
@@ -0,0 +1,270 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Des 2024 pada 09.53
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fti_campus_hub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `kegiatan_id` int(11) NOT NULL,
+  `penyelenggara_id` int(11) NOT NULL,
+  `nama_kegiatan` varchar(100) NOT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tanggal_mulai` date NOT NULL,
+  `tanggal_selesai` date NOT NULL,
+  `batas_pendaftaran` date NOT NULL,
+  `kuota` int(11) NOT NULL,
+  `status` enum('draft','published','closed','finished') NOT NULL,
+  `lokasi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `panitia_kegiatan`
+--
+
+CREATE TABLE `panitia_kegiatan` (
+  `panitia_kegiatan_id` int(11) NOT NULL,
+  `kegiatan_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `jabatan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftaran`
+--
+
+CREATE TABLE `pendaftaran` (
+  `user_id` int(11) NOT NULL,
+  `kegiatan_id` int(11) NOT NULL,
+  `status` enum('accepted','rejected','pending') NOT NULL,
+  `alamat` text DEFAULT NULL,
+  `jabatan` varchar(100) DEFAULT NULL,
+  `alasan_pendaftaran` text DEFAULT NULL,
+  `waktu_daftar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengumuman`
+--
+
+CREATE TABLE `pengumuman` (
+  `pengumuman_id` int(11) NOT NULL,
+  `kegiatan_id` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `waktu_pengumuman` datetime NOT NULL,
+  `file_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penyelenggara`
+--
+
+CREATE TABLE `penyelenggara` (
+  `penyelenggara_id` int(11) NOT NULL,
+  `nama_penyelenggara` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `no_telp` varchar(15) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `logo_path` varchar(255) DEFAULT NULL,
+  `jenis` enum('fakultas','jurusan','himpunan','ukm') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sertifikat`
+--
+
+CREATE TABLE `sertifikat` (
+  `sertifikat_id` int(11) NOT NULL,
+  `panitia_kegiatan_id` int(11) NOT NULL,
+  `nomor_sertifikat` varchar(100) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `tanggal_terbit` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nim` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `no_telp` varchar(15) DEFAULT NULL,
+  `role` enum('mahasiswa','panitia') NOT NULL,
+  `pp_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD PRIMARY KEY (`kegiatan_id`),
+  ADD KEY `penyelenggara_id` (`penyelenggara_id`);
+
+--
+-- Indeks untuk tabel `panitia_kegiatan`
+--
+ALTER TABLE `panitia_kegiatan`
+  ADD PRIMARY KEY (`panitia_kegiatan_id`),
+  ADD KEY `kegiatan_id` (`kegiatan_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeks untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`user_id`,`kegiatan_id`),
+  ADD KEY `kegiatan_id` (`kegiatan_id`);
+
+--
+-- Indeks untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD PRIMARY KEY (`pengumuman_id`),
+  ADD KEY `kegiatan_id` (`kegiatan_id`);
+
+--
+-- Indeks untuk tabel `penyelenggara`
+--
+ALTER TABLE `penyelenggara`
+  ADD PRIMARY KEY (`penyelenggara_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeks untuk tabel `sertifikat`
+--
+ALTER TABLE `sertifikat`
+  ADD PRIMARY KEY (`sertifikat_id`),
+  ADD UNIQUE KEY `nomor_sertifikat` (`nomor_sertifikat`),
+  ADD KEY `panitia_kegiatan_id` (`panitia_kegiatan_id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `nim` (`nim`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  MODIFY `kegiatan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `panitia_kegiatan`
+--
+ALTER TABLE `panitia_kegiatan`
+  MODIFY `panitia_kegiatan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  MODIFY `pengumuman_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penyelenggara`
+--
+ALTER TABLE `penyelenggara`
+  MODIFY `penyelenggara_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `sertifikat`
+--
+ALTER TABLE `sertifikat`
+  MODIFY `sertifikat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD CONSTRAINT `kegiatan_ibfk_1` FOREIGN KEY (`penyelenggara_id`) REFERENCES `penyelenggara` (`penyelenggara_id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `panitia_kegiatan`
+--
+ALTER TABLE `panitia_kegiatan`
+  ADD CONSTRAINT `panitia_kegiatan_ibfk_1` FOREIGN KEY (`kegiatan_id`) REFERENCES `kegiatan` (`kegiatan_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `panitia_kegiatan_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `pendaftaran_ibfk_2` FOREIGN KEY (`kegiatan_id`) REFERENCES `kegiatan` (`kegiatan_id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pengumuman`
+--
+ALTER TABLE `pengumuman`
+  ADD CONSTRAINT `pengumuman_ibfk_1` FOREIGN KEY (`kegiatan_id`) REFERENCES `kegiatan` (`kegiatan_id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `sertifikat`
+--
+ALTER TABLE `sertifikat`
+  ADD CONSTRAINT `sertifikat_ibfk_1` FOREIGN KEY (`panitia_kegiatan_id`) REFERENCES `panitia_kegiatan` (`panitia_kegiatan_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
