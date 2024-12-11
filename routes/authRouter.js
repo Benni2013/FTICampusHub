86905711e6fe726e910c4db323606cadee3ef354
Benni Putra Chaniago <bennichaniago2003@gmail.com>
2024-12-11 23:28:40 +0700

    tinggal sertif

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,7 @@
 // Router Authentication
 var express = require('express');
 var router = express.Router();
+const { Users } = require('../models/RelasiTabel');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +14,22 @@ router.get('/register', function(req, res, next) {
 
 router.get('/forgot_password', function(req, res, next) {
   res.render('./auth/forgot_password.hbs', { title: 'Forgot Password' });
+});
+
+router.get('/profile', async (req, res, next) => {
+  try {
+      let user_id = req.query.user;
+      let user = await Users.findOne({ where: { user_id } });
+
+      res.render('./profile.hbs', { 
+          title: 'Profile', 
+          layout: "layouts/main",
+          data: { user }
+      });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error occurred' });
+  }
 });
 
 module.exports = router;
